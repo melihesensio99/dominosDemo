89569258951c69977920f7d8f96358e2db0bb09e
melo <melih@DESKTOP-MRA5AHM>
2026-07-26 18:51:07 +0300

    Auth gelistirme ortaminda login baslangici duzenlendi
    Gelistirme ortaminda bozuk kullanici profili anahtarlarinin Auth servisini etkilemesi engellendi. Gecici DataProtection provider ve yalnizca konsol loglamasi kullanilarak servis baslangici ve Gateway uzerinden login hatalarinin duzgun HTTP cevabi vermesi saglandi.

--- src/Services/Auth/Auth.Api/Program.cs
+++ src/Services/Auth/Auth.Api/Program.cs
@@ -1,7 +1,15 @@
 using Auth.Api.Features;
 using BuildingBlocks.Web;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Logging.ClearProviders();
+    builder.Logging.AddConsole();
+    builder.Services.AddDataProtection().UseEphemeralDataProtectionProvider();
+}
 
 AuthModule.ConfigureServices(builder.Services, builder.Configuration);
 
